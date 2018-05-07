66714d219fd594ae4e0109e913526deb4fecfd02
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-07 06:37:48 +0000

    Deploy claustres/nature-aventure to github.com/claustres/nature-aventure.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "18e9e3bbbd7d4e97f133223776d9e073"
+    "revision": "9073096e21a49682a89f0164e6f81388"
   },
   {
     "url": "accueil.png",
