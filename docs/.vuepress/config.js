1cccc21da2b30589a848ca8edfc76b83053ceb3f
Luc Claustres <luc.claustres@orange.fr>
2018-10-22 16:54:53 +0200

    Fixed base URL to work with custom domain

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   dest: 'site',
-  base: '/nature-aventure/',
+  base: '/',
   title: 'Club Nature-Aventure',
   description: 'Activités de pleine nature à Castelnaudary',
   head: [
