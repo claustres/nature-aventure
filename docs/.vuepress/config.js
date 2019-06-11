84a3ddfd7d55b5fa07576686319457bedc153426
Luc Claustres <luc.claustres@orange.fr>
2019-06-11 23:01:31 +0200

    Added PWA plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,6 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
-  serviceWorker: true,
   theme: 'nature-aventure',
   themeConfig: {
     docsDir: 'docs',
