9423c0188f3e7bbe156f87ea9c57aca2155135fc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-06-09 06:42:31 +0000

    Deploy claustres/nature-aventure to github.com/claustres/nature-aventure.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "0e03602f859096f524dc0dde0925eb3a"
+    "revision": "64af031c3f284977d932da3f13339a2c"
   },
   {
     "url": "accueil.png",
@@ -29,7 +29,7 @@ self.__precacheManifest = [
   },
   {
     "url": "adherer/index.html",
-    "revision": "b258d5ce78e6cb77e1c507bd0b9aa3b2"
+    "revision": "13c490b6f35e08d1959e8eecd4f4e6a9"
   },
   {
     "url": "assets/css/0.styles.d794acf3.css",
@@ -52,8 +52,8 @@ self.__precacheManifest = [
     "revision": "164088435d5507df6fed12f7dd543ba2"
   },
   {
-    "url": "assets/js/11.cd8890df.js",
-    "revision": "06b6b15b9183e42b0bc7f14d8fd69136"
+    "url": "assets/js/11.9712d542.js",
+    "revision": "60777a43b7f64f17260ed6b067e30a16"
   },
   {
     "url": "assets/js/12.7cf1ce61.js",
@@ -92,16 +92,16 @@ self.__precacheManifest = [
     "revision": "06284aa0046189aa1879def16772b362"
   },
   {
-    "url": "assets/js/app.7963b03b.js",
-    "revision": "76d8167e8e82c83963834ff99f042e78"
+    "url": "assets/js/app.9a3c3f18.js",
+    "revision": "f59ee7816c079ba9e295b193dd2a6b58"
   },
   {
     "url": "club/index.html",
-    "revision": "7833ba88a740c1203bcc445d05310262"
+    "revision": "04b34b76cb3f764bc54014cc29b46ea2"
   },
   {
     "url": "contact/index.html",
-    "revision": "85ca3b6e9ba0f75d644d3f456bc3df84"
+    "revision": "2b52a42feca4721633ee07130cf42ff2"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -121,7 +121,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "04002e4135f23053d936e37989024f4b"
+    "revision": "af4c022200caccc4a7b3ce3eceeb001f"
   },
   {
     "url": "logo_aude.jpg",
@@ -149,11 +149,11 @@ self.__precacheManifest = [
   },
   {
     "url": "planning/index.html",
-    "revision": "d4a2a4838d45e4a609466623558f715a"
+    "revision": "06632eb425e56e562fb37d50947a276e"
   },
   {
     "url": "pratique/index.html",
-    "revision": "caa27c940bd176879ac68d42c799ad48"
+    "revision": "d048d3e7012710420ce1b03510292be4"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
